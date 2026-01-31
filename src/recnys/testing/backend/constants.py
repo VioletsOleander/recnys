4155cf6e65e28043f856fcae8fc7aade53292f75
VioletsOleander <1377232072@qq.com>
2026-01-31 21:24:55 +0800

    feat: support de-duplication in task canonicalization

--- src/recnys/testing/backend/constants.py
+++ src/recnys/testing/backend/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from recnys.backend.task import CanonicalSyncTask
 from recnys.frontend.task import Policy
-from recnys.testing.frontend.utils import SrcAttr, make_sync_task
+from recnys.testing.frontend.utils import SrcAttr, SyncTaskAttr, make_sync_task
 
 __all__ = [
     "CANONICALIZED_SYNC_TASKS",
@@ -15,55 +15,64 @@ __all__ = [
 SRC_CONTENT = "Sample content for source files."
 DST_CONTENT = "Sample content for destination files."
 
-_SRC_ATTRS = (
-    SrcAttr(path=Path("/source/file_with_dest"), is_dir=False),
-    SrcAttr(path=Path("/source/file_without_dest"), is_dir=False),
-    SrcAttr(path=Path("/source/dir_with_dest/"), is_dir=True),
-    SrcAttr(path=Path("/source/dir_without_dest/"), is_dir=True),
+_SYNC_TASK_ATTRS = (
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/file"), is_dir=False),
+        dst=Path("/destination/file"),
+        policy=Policy.SOURCE,
+    ),
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/file"), is_dir=False),
+        dst=Path("/destination/file"),
+        policy=Policy.OVERWRITE,
+    ),
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/file_no_sync"), is_dir=False),
+        dst=None,
+        policy=Policy.SOURCE,
+    ),
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/dir/"), is_dir=True),
+        dst=Path("/destination/dir/"),
+        policy=Policy.OVERWRITE,
+    ),
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/dir_no_sync/"), is_dir=True),
+        dst=None,
+        policy=Policy.SOURCE,
+    ),
+    SyncTaskAttr(
+        src=SrcAttr(path=Path("/source/dir/file1.txt"), is_dir=False),
+        dst=Path("/destination/dir/file1.txt"),
+        policy=Policy.SOURCE,
+    ),
 )
 
-_DST_PATHS = (
-    Path("/destination/file_with_dest"),
-    None,
-    Path("/destination/dir_with_dest/"),
-    None,
-)
-
-_POLICIES = (
-    Policy.OVERWRITE,
-    Policy.DEFAULT,
-    Policy.SOURCE,
-    Policy.DEFAULT,
-)
-
-PARSED_SYNC_TASKS = [
-    make_sync_task(src_attr, dst_path, policy)
-    for src_attr, dst_path, policy in zip(_SRC_ATTRS, _DST_PATHS, _POLICIES, strict=True)
-]
+PARSED_SYNC_TASKS = [make_sync_task(sync_task_attr=attr) for attr in _SYNC_TASK_ATTRS]
 
 FILES_UNDER_DIR = (
-    Path("/source/dir_with_dest/file1.txt"),
-    Path("/source/dir_with_dest/file2.txt"),
-    Path("/source/dir_with_dest/subdir/file3.txt"),
+    Path("/source/dir/file1.txt"),
+    Path("/source/dir/file2.txt"),
+    Path("/source/dir/subdir/file3.txt"),
 )
 
 
 def _canonicalized_sync_tasks() -> list[CanonicalSyncTask]:
-    tasks = []
-    for src_attr, dst_path, policy in zip(_SRC_ATTRS, _DST_PATHS, _POLICIES, strict=True):
-        if not src_attr.is_dir:
-            if dst_path is None:
-                continue
-            tasks.append(CanonicalSyncTask(src_attr.path, dst_path, policy))
-        else:
-            if dst_path is None:
-                continue
-            for file_path in FILES_UNDER_DIR:
-                relative_path = file_path.relative_to(src_attr.path)
-                effective_dst_path = dst_path / relative_path
-                tasks.append(CanonicalSyncTask(file_path, effective_dst_path, policy))
+    tasks: dict[Path, CanonicalSyncTask] = {}
 
-    return tasks
+    for src, dst, policy in _SYNC_TASK_ATTRS:
+        if dst is None:
+            continue
+
+        if not src.is_dir:
+            tasks[src.path] = CanonicalSyncTask(src.path, dst, policy)
+        else:
+            for file_path in FILES_UNDER_DIR:
+                relative_path = file_path.relative_to(src.path)
+                effective_dst_path = dst / relative_path
+                tasks[file_path] = CanonicalSyncTask(file_path, effective_dst_path, policy)
+
+    return list(tasks.values())
 
 
 CANONICALIZED_SYNC_TASKS = _canonicalized_sync_tasks()
