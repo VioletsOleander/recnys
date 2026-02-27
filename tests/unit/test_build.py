cb525d436351ff1b12bf88eed62458aca8c3c872
VioletsOleander <1377232072@qq.com>
2026-02-27 08:56:34 +0800

    update

--- tests/unit/test_build.py
+++ tests/unit/test_build.py
@@ -1,8 +1,11 @@
+import pytest
+
 from recnys.build import build_render_tasks, build_sync_tasks
 from recnys.testing.build.arrange import make_render_tasks, make_sync_tasks
 from recnys.testing.canonicalize.arrange import make_canonical_config
 
 
+@pytest.mark.usefixtures("filesystem")
 def test_build_sync_tasks(system: str) -> None:
     expected_sync_tasks = make_sync_tasks(system=system)
     canonical_config = make_canonical_config(system=system)
@@ -12,6 +15,7 @@ def test_build_sync_tasks(system: str) -> None:
     assert result == expected_sync_tasks
 
 
+@pytest.mark.usefixtures("filesystem")
 def test_build_render_tasks(system: str) -> None:
     expected_render_tasks = make_render_tasks(system=system)
     canonical_config = make_canonical_config(system=system)
