bee2699559b92d1b37da5bcc0f2e9107761ea21d
VioletsOleander <1377232072@qq.com>
2026-04-12 12:23:28 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
-import platform
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
 from pyfakefs.fake_filesystem import FakeFilesystem, OSType
+
+if TYPE_CHECKING:
+    from pytest_mock import MockFixture
 
 
 @pytest.fixture
@@ -11,8 +14,8 @@ def resources_dir(pytestconfig: pytest.Config) -> Path:
 
 
 @pytest.fixture(params=["Linux", "Windows"])
-def system(monkeypatch: pytest.MonkeyPatch, request: pytest.FixtureRequest) -> str:
-    monkeypatch.setattr(platform, "system", lambda: request.param)
+def system(mocker: MockFixture, request: pytest.FixtureRequest) -> str:
+    mocker.patch("recnys.tree.ctree.builder.platform.system", return_value=request.param)
     return request.param
 
 
