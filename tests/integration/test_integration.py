9665b41d0d3f74707265e9818bbb403d27a11f28
Xinjie Chen <1377232072@qq.com>
2026-06-25 22:41:42 +0800

    test: implement primitive integration test (#78)
    Also:
    
    - refactor to simplify the test structure
    - use dprint for markdown check
    - move project-words to cspell.json to reduce the number of files

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,21 +1,49 @@
 from argparse import Namespace
+from pathlib import Path
 from typing import TYPE_CHECKING
 
+import pytest
+
 from recnys.main import main
-from recnys.testing.arranger import create_config_files, create_source_files, setup_cwd
-from recnys.testing.integration.constants import SOURCE_FILES
+from recnys.testing.integration.arranger import create_config_files
+from recnys.testing.integration.constants import LINUX_TARGETS, SOURCES, WINDOWS_TARGETS
+from recnys.testing.node import File, Symlink
 
 if TYPE_CHECKING:
-    from pathlib import Path
-
     from pyfakefs.fake_filesystem import FakeFilesystem
 
 
-def test_integration(resources_dir: Path, filesystem: FakeFilesystem) -> None:
-    setup_cwd(filesystem)
-    create_config_files(filesystem, resources_dir=resources_dir)
-    create_source_files(filesystem, source_files=SOURCE_FILES)
+@pytest.fixture
+def recnys_file(pytestconfig: pytest.Config) -> Path:
+    return pytestconfig.rootpath / "tests" / "integration" / "resources" / "recnys.yaml"
+
+
+@pytest.fixture
+def variables_file(pytestconfig: pytest.Config) -> Path:
+    return pytestconfig.rootpath / "tests" / "integration" / "resources" / "variables.yaml"
+
+
+def test_integration(
+    filesystem: FakeFilesystem, system: str, recnys_file: Path, variables_file: Path
+) -> None:
     args = Namespace(silent=True, debug=False, dry_run=False)
 
-    # Un-decorate the main function
+    create_config_files(filesystem, recnys_file=recnys_file, variables_file=variables_file)
+    for s in SOURCES:
+        filesystem.create_file(s.path, contents=s.content)
+
+    # Undecorate the main function
     main.__wrapped__(args)  # type: ignore[ty:unresolved-attribute]
+
+    match system:
+        case "Linux":
+            targets = LINUX_TARGETS
+        case "Windows":
+            targets = WINDOWS_TARGETS
+
+    for t in targets:
+        if isinstance(t, File):
+            content = (Path.home() / t.path).read_text()
+            assert content == t.content
+        elif isinstance(t, Symlink):
+            assert (Path.home() / t.dst).readlink() == Path(t.src).resolve()
